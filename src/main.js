d82373d8f23823fab008a0df3207a2d04ebf2f8d
Michael Gerver <mgerver@ufl.edu>
2020-03-14 17:19:58 -0400

    Add js code to call includeSpoon method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// Create a class called Desserts with two properties
 class Desserts {
     constructor(type, calories) {
         this.type = type;
@@ -5,21 +6,26 @@ class Desserts {
     }
 }    
 
+// Create a subclass called IceCream that extends Desserts with two additional properties
     class IceCream extends Desserts {
         constructor(type, calories, flavor, scoops) {
             super(type, calories); 
                 this.flavor = flavor;
                 this.scoops = scoops;
             }
-
+            
+// Create a method
         includeSpoon() {
            if(this.type === 'Ice Cream') {
             console.log('Here is your spoon!');
            }
         }
     }
-
+// Create a variable that's a new instance of IceCream
 const vanillaIceCream = new IceCream('Ice Cream',340,'Vanilla',3);
 
 console.log(vanillaIceCream);
+
+// Call includeSpoon method
+vanillaIceCream.includeSpoon();
 
